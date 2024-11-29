9aa57154e9a756eb4c86f04d12340ee7087eee68
sicot <sicotq@gmail.com>
2024-11-29 17:35:06 +0100

    je commence les

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,4 +1,5 @@
 import {Component, inject, OnInit, Renderer2} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-carte',
@@ -10,10 +11,27 @@ import {Component, inject, OnInit, Renderer2} from '@angular/core';
 export class CarteComponent implements OnInit {
   renderer = inject(Renderer2);
   ngOnInit() {
+    // Ajout d'un link vers le CSS de la carte interactive
     const link = this.renderer.createElement('link');
     this.renderer.setAttribute(link, 'rel', 'stylesheet');
     this.renderer.setAttribute(link, 'href', "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css");
     this.renderer.setAttribute(link, 'integrity', "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=");
     this.renderer.setAttribute(link, 'crossOrigin', "");
+
+    this.renderer.appendChild(document.head, link);
+
+    // Ajout d'une balise script pour ajouter le code gérant la carte interactive
+    const script = this.renderer.createElement('script');
+    script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
+    script.integrity = "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=";
+    script.crossOrigin = "";
+
+    this.renderer.appendChild(document.head, script);
+
+    var map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
   }
 }
