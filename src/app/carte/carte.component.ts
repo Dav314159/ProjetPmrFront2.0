8de58c429b9c5e859a2305dbd9648f63b8089313
sicot <sicotq@gmail.com>
2024-12-02 14:10:11 +0100

    La map affiche les PMR

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,5 +1,8 @@
-import {Component, inject, OnInit, Renderer2} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, Renderer2} from '@angular/core';
 import * as L from 'leaflet';
+import {Observable, Subscriber} from "rxjs";
+import {PmrService} from "../services/pmr-service/pmr.service";
+import {Pmr} from "../models/Pmr";
 
 @Component({
   selector: 'app-carte',
@@ -8,30 +11,81 @@ import * as L from 'leaflet';
   templateUrl: './carte.component.html',
   styleUrl: './carte.component.css'
 })
-export class CarteComponent implements OnInit {
-  renderer = inject(Renderer2);
+export class CarteComponent implements OnInit, AfterViewInit {
+  map: any;
+  pmrService = inject(PmrService);
+  dataPmr = new Array<Pmr>();
+
+
   ngOnInit() {
-    // Ajout d'un link vers le CSS de la carte interactive
-    const link = this.renderer.createElement('link');
-    this.renderer.setAttribute(link, 'rel', 'stylesheet');
-    this.renderer.setAttribute(link, 'href', "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css");
-    this.renderer.setAttribute(link, 'integrity', "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=");
-    this.renderer.setAttribute(link, 'crossOrigin', "");
+  }
 
-    this.renderer.appendChild(document.head, link);
+  ngAfterViewInit() {
+    this.loadMap();
+    this.pmrService.getAllPmr().subscribe({
+      next: data => {
+        this.dataPmr = data;
+        this.updatePmrMarkers();
+      },
+      error: error => {},
+    })
+  }
 
-    // Ajout d'une balise script pour ajouter le code gérant la carte interactive
-    const script = this.renderer.createElement('script');
-    script.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
-    script.integrity = "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=";
-    script.crossOrigin = "";
+  getCurrentPosition(): any {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: any) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          observer.complete();
+        });
+      } else {
+        observer.error();
+      }
+    });
+  }
 
-    this.renderer.appendChild(document.head, script);
-
-    var map = L.map("map").setView([51.505, -0.09], 13);
+  loadMap(): void {
+    this.map = L.map('map').setView([0, 0], 1);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    }).addTo(this.map);
+
+    this.getCurrentPosition()
+      .subscribe((position: any) => {
+        this.map.flyTo([position.latitude, position.longitude], 13);
+
+        const icon = L.icon({
+          iconUrl: 'assets/images/marker-icon.png',
+          shadowUrl: 'assets/images/marker-shadow.png',
+          popupAnchor: [13, 0],
+        });
+
+        // Dans le bindPopup, on peut mettre du code html dedans
+        const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Angular Leaflet');
+        marker.addTo(this.map);
+      });
+  }
+
+  updatePmrMarkers(): void {
+    // On supprimer tous les markers ayant pu être mis, avant de les mettre à jour ce qui les réaffichera
+
+    for (const pmr of this.dataPmr) {
+      const pos: string[] = pmr.point_geo.split(", ");
+      const markerPmr = L.marker([Number(pos[0]), Number(pos[1])]);
+      markerPmr.bindPopup( `${pmr.nom}, ${pmr.description}, ${pmr.quantite}`);
+      markerPmr.addTo(this.map);
+    }
+  }
+
+  clearAllMarkers(): void {
+    this.map.eachLayer((layer: any) => {
+      if (layer instanceof L.Marker) {
+        this.map.removeLayer(layer);
+      }
+    })
   }
 }
