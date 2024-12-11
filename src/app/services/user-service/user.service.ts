d8ebeddef8736baf2d991798e62183f9f72daf05
Drenmax7 <maxendrecoulon@gmail.com>
2024-12-11 21:17:27 +0100

    ajout declaration de fonctions pour la connexion/inscription

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../../models/User";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  API_URL = "http://localhost:8080";
+  API_ENTITY_NAME = "utilisateur";
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
+
+  checkPassword(username : string, password : string): boolean
+  {
+    //todo hash password
+    console.log(username, password);
+    return true;
+    //return this.http.get<User>(`${this.API_URL}/${this.API_ENTITY_NAME}/getPmr?id=${id}`);
+  }
+
+  //renvoie si l'ajout s'est bien effectue
+  addUser(data : User) : boolean{
+    return false;
+  }
+
+  checkUsername(username : string) : boolean{
+    return true;
+  }
+
 }
