3d3297eb72a9243f36e758cef0f3a13bb2c63f9f
Drenmax7 <maxendrecoulon@gmail.com>
2024-12-13 15:33:40 +0100

    ajout de la visualisation et modification des infos de l'user

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,22 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {userLogin} from "../../main";
 import {Router} from "@angular/router";
+import {ChampsComponent} from "../connexion/champs/champs.component";
+import {NgIf} from "@angular/common";
+import {ConnexionComponent} from "../connexion/connexion.component";
+import {UserService} from "../services/user-service/user.service";
 
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [],
+  imports: [
+    ChampsComponent,
+    NgIf,
+    ConnexionComponent
+  ],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.css'
 })
 export class ProfilComponent {
-  protected readonly userLogin = userLogin;
+  userService = inject(UserService);
+
 
   constructor(private router: Router) {
     if (userLogin.username == "") {
       this.router.navigate(['/connexion']);
     }
+    this.remplirChamps();
   }
+
+  remplirChamps(){
+    this.userService.getUser().subscribe({
+      next: user => {
+        console.log("user recu");
+        let username: HTMLInputElement = <HTMLInputElement>document.getElementById("username");
+        let password: HTMLInputElement = <HTMLInputElement>document.getElementById("password");
+        let mail: HTMLInputElement = <HTMLInputElement>document.getElementById("mail");
+        let prenom: HTMLInputElement = <HTMLInputElement>document.getElementById("firstname");
+        let nom: HTMLInputElement = <HTMLInputElement>document.getElementById("lastname");
+
+        username.value = user.username;
+        password.value = user.password;
+        mail.value = user.mail;
+        prenom.value = user.prenom;
+        nom.value = user.nom;
+      },
+      error: error =>{
+        console.log("user non recu");
+      }
+
+    });
+  }
+
+
 
 }
